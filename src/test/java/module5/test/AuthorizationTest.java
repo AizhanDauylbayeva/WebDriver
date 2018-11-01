36499552fbc76af50822c34f41748ba95f1e8673
Aizhan Dauylbayeva <Aizhan_Dauylbayeva@epam.com>
2018-11-01 16:02:29 +0600

    modified

--- src/test/java/module5/test/AuthorizationTest.java
+++ src/test/java/module5/test/AuthorizationTest.java
@@ -35,8 +35,10 @@ public class AuthorizationTest extends BeforeSuite {
         addressee.sendKeys("ayzhan7797@mail.ru");
         WebElement subject = driver.findElement(By.xpath("//input[@class='b-input' ]"));
         subject.sendKeys("test(module 5)");
-      //  WebElement body = driver.findElement(By.cssSelector("#tinymce"));
-      //  body.sendKeys("Hello!");
+        driver.switchTo().frame(0);
+        WebElement body = driver.findElement(By.xpath("html/body[@id='tinymce']"));
+        body.sendKeys("Hello!");
+        driver.switchTo().defaultContent();
     }
 
     @Test(dependsOnMethods = "createNewMail")
@@ -45,14 +47,59 @@ public class AuthorizationTest extends BeforeSuite {
     }
 
     @Test(dependsOnMethods = "saveTheMail")
-    public void verifyTheMailIsSaved(){
+    public void testTheMailIsSaved(){
         boolean isSaved = driver.findElement(By.xpath("//a[@class='toolbar__message_info__link']")).isDisplayed();
         Assert.assertTrue(isSaved);
     }
 
-    @Test(dependsOnMethods = "verifyTheMailIsSaved")
-    public void verifyDraftsFolder(){
-        driver.findElement(By.xpath("//div[@class='b-nav__item js-href b-nav__item_active']")).click();
+    @Test(dependsOnMethods = "testTheMailIsSaved")
+    public void testDraftsFolder() {
+        driver.findElement(By.xpath("//div[@class='b-toolbar__message']/a")).click();
+    }
 
+    @Test(dependsOnMethods = "testDraftsFolder")
+    public void testTheDraftContent(){
+        WebElement addressee = driver.findElement(By.xpath("//div[@class='b-datalist__item__addr' and contains(string(), 'ayzhan7797@mail.ru')]"));
+        boolean addr = addressee.isDisplayed();
+        Assert.assertTrue(addr);
+        addressee.click();
+
+        WebElement subject = driver.findElement(By.xpath("//a[@data-subject = 'test(module 5)']")); //
+        boolean subj = subject.isEnabled();
+        Assert.assertTrue(subj);
+
+        driver.switchTo().frame(0);
+        boolean body = driver.findElement(By.xpath("//*[@class = 'js-helper js-readmsg-msg' and contains(string(), 'Hello!')]")).isDisplayed();
+        Assert.assertTrue(body);
+        driver.switchTo().defaultContent();
+    }
+
+    @Test(dependsOnMethods = "testTheDraftContent")
+    public void sendTheMail(){
+        driver.findElement(By.xpath("//div[@data-name='send']/span")).click();
+    }
+
+    @Test(dependsOnMethods = "sendTheMail")
+    public void testTheMailDisappearedFromDrafts(){
+        driver.findElement(By.xpath("//a[@data-mnemo='drafts']")).click();
+        driver.navigate().refresh();
+        try{
+            driver.findElement(By.xpath("//*[@class='b-datalist__item__subj' and contains(string(), 'test(module 5)')]"));
+            System.err.println("The mail still in ‘Drafts’ folder");
+        } catch (Exception e){
+            System.out.println("The mail successfully disappeared from ‘Drafts’ folder");
+        }
+    }
+
+    @Test(dependsOnMethods = "testTheMailDisappearedFromDrafts")
+    public void testTheMailIsInSentFolder(){
+        driver.findElement(By.xpath("//a[@href='/messages/sent/']")).click();
+        boolean subj = driver.findElement(By.xpath("//*[@class='b-datalist__item__subj' and contains(string(), 'test(module 5)')]")).isDisplayed();
+        Assert.assertTrue(subj);
+    }
+
+    @Test(dependsOnMethods = "testTheMailIsInSentFolder")
+    public void logout(){
+        driver.findElement(By.id("PH_logoutLink")).click();
     }
 }
